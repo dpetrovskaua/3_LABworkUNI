d17af9188079570f26a586ce265a63bc20554dd2
DianaPetrovska <259563350+dpetrovskaua@users.noreply.github.com>
2026-04-17 19:12:34 +0300

    Update main.py

--- lab3FILES/main.py
+++ lab3FILES/main.py
@@ -1,5 +1,4 @@
 from base import BuildingBlock
-
 def main():
     blocks = [
         BuildingBlock("Stone", "Structural", 1.5, 6.0, False),
@@ -11,27 +10,24 @@ def main():
         BuildingBlock("Sand", "Natural", 0.5, 0.5, False),
     ]
 
-    print("--- Original Array ---")
+    print("Оригінальний порядок:")
     for block in blocks:
         print(block)
-    # сортування
-    # мінус перед b.blast_resistance дає спадання.
+    # сортування; мінус перед b.blast_resistance дає спадання.
     blocks.sort(key=lambda b: (b.hardness, -b.blast_resistance))
+    print("\nВідсортований порядок (За зростанням по hardness, За спаданням по Blast resistance):")
 
-    print("\n--- Sorted Array (Hardness ASC, Blast resistance DESC) ---")
     for block in blocks:
         print(block)
-    # пошук ідент об'єктів
-    # беремо рандом об'єкт: скло
+    # пошук ідент об'єктів; створюємо об'єкт з такими ж характеристиками, як у скла
     target_block = BuildingBlock("Glass", "Ornamental", 0.3, 0.3, True)
 
-    print(f"\n--- Searching for: {target_block} ---")
-    # через __eq__ в класі, 'in' порівняє всі поля, а не адреси в пам'яті
+    print(f"\nПошук: {target_block}")
     if target_block in blocks:
         index = blocks.index(target_block)
-        print(f"Success! Identical object found at index: {index}")
+        print(f"Успіх! Ідентичний блок знайдено за номером: {index}")
     else:
-        print("Object not found in the array.")
+        print("Блок не знайдено.")
 
 if __name__ == "__main__":
     main()
